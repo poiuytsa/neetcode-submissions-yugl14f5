7c9a54df3e209847ac466fd240a0f5751b21a739
poiuytsa <103122922+poiuytsa@users.noreply.github.com>
2026-07-18 14:55:23 +0530

    Add: generate-parentheses - submission-17

---
+++ Data Structures & Algorithms/generate-parentheses/submission-17.py
@@ -0,0 +1,24 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res=[]
+        subset=[]
+        def dfs(op,clo):
+            if len(subset)==2*n and op==clo:
+                res.append("".join(subset))
+                return 
+            if op<clo or op>n:
+                return 
+
+            if op<n:
+                subset.append('(')
+                dfs(op+1,clo)
+                subset.pop()
+
+            if clo<op:
+                subset.append(')')
+                dfs(op,clo+1)
+                subset.pop()
+
+        dfs(0,0)
+        return res
+
