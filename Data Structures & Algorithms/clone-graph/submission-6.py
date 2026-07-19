a5e230294913f0c7f6db43407149af9b29eb7405
poiuytsa <103122922+poiuytsa@users.noreply.github.com>
2026-07-19 08:35:47 +0530

    Add: clone-graph - submission-6

---
+++ Data Structures & Algorithms/clone-graph/submission-6.py
@@ -0,0 +1,57 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        # 2 pass sol
+        '''
+        if not node:
+            return None 
+
+        mapping={}
+        visited=set()
+        def dfs(node):
+            if node in visited:
+                return 
+            visited.add(node)
+            mapping[node]=Node(node.val)
+            for n in node.neighbors:
+                dfs(n)
+        dfs(node)
+        
+
+        visited2=set()
+        def dfs2(node):
+            if node in visited2:
+                return
+            visited2.add(node)
+            for n in node.neighbors:
+                mapping[node].neighbors.append(mapping[n])
+            for n in node.neighbors:
+                dfs2(n)
+        
+        dfs2(node)
+
+        return mapping[node]
+        '''
+
+
+        mapping={}
+        if not node:
+            return None 
+
+        def dfs(node):
+            if node in mapping:
+                return mapping[node]
+            
+            mapping[node]=Node(node.val)
+            for n in node.neighbors:
+                mapping[node].neighbors.append(dfs(n))
+            return mapping[node]
+        
+        return dfs(node)
